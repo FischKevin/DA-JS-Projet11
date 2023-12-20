f025e497db82aca96053320a5ab99ca6ffc2a0dd
Kévin Fischer <kevinfischer@live.fr>
2023-12-20 09:24:43 +0100

    :sparkles: added banner

---
+++ src/components/Banner.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../sass/components/_banner.scss';
+
+function Banner({ image, text }) {
+  return (
+    <div className="BannerImage" style={{ backgroundImage: `url(${image})` }}>
+      <h1 className="BannerText">{text}</h1>
+    </div>
+  );
+}
+
+Banner.propTypes = {
+  image: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired
+};
+
+export default Banner;
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import Header from './Header';
+import Banner from './Banner';
 
 function HomePage() {
-  return <Header></Header>;
+  return (
+    <>
+      <Header />
+      <Banner 
+        image="src/assets/top_img.png"
+        text="Chez vous, partout et ailleurs"
+      />
+    </>
+  );
 }
 
 export default HomePage;
