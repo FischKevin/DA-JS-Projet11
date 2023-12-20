cb3ad519de4f0e19bb7ab0bdd0c0d8fff9c6cfbe
Kévin Fischer <kevinfischer@live.fr>
2023-12-20 17:17:33 +0100

    :sparkles: added 404 error page

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
 
 function Error404() {
   return (
-    <div>
-      <h1>404 - Page non trouvée</h1>
-      <p>Désolé, la page que vous recherchez n existe pas.</p>
-      <Link to="/">Retourner à l accueil</Link>
-    </div>
+    <>
+      <Header />
+      <div className="error-content">
+        <h1 className='title404'>404</h1>
+        <p className='text404'>Oups! La page que vous demandez n&apos;existe pas.</p>
+        <Link to="/" className='link404'>Retourner sur la page d&apos;accueil</Link>
+      </div>
+      <Footer />
+    </>
   );
 }
 
