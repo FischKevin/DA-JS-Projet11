d17c3d1dae32e4dccf562e156ffd798502e0d123
Kévin Fischer <kevinfischer@live.fr>
2023-12-22 15:40:03 +0100

    :bug: fix to underline active link instead of hover link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,9 +4,9 @@ import '../sass/components/_navbar.scss';
 
 function NavBar() {
   return (
-    <nav className="NavBar">
-      <NavLink to="/">Accueil</NavLink>
-      <NavLink to="/APropos">A Propos</NavLink>
+    <nav className='NavBar'>
+      <NavLink to="/" className={({ isActive }) => isActive ? 'active' : undefined}>Accueil</NavLink>
+      <NavLink to="/APropos" className={({ isActive }) => isActive ? 'active' : undefined}>A Propos</NavLink>
     </nav>
   );
 }
